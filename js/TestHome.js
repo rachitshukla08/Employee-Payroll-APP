98791b0307f38e1d7da1186bd9891230e5e848e4
unknown <rachitshukla08@gmail.com>
2020-11-17 15:11:39 +0530

    Added: Template literals and for loop to populate innerHTML of table

--- js/TestHome.js
+++ js/TestHome.js
@@ -5,23 +5,63 @@ window.addEventListener('DOMContentLoaded',(event)=>{
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"
                         +"<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-    <tr>
-        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png">
-    </td>
-    <td>Narayan</td>
-    <td>Male</td>
-    <td><div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
-    <td>300000</td>
-    <td>1 Nov 2020</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete"
-            src="../assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" 
-            src="../assets/icons/create-black-18dp.svg">
-    </td>
-    </tr>
-`;
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSON();
+    for(const employeePayrollData of employeePayrollList){
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" alt="" src="${employeePayrollData._profilePic}">
+        </td>
+        <td>${employeePayrollData._name}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td>${getDeptHtml(employeePayrollData._department)}</td>
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
+        <td>
+            <img id="1" onclick="remove(this)" alt="delete"
+                src="../assets/icons/delete-black-18dp.svg">
+            <img id="1" alt="edit" onclick="update(this)" 
+                src="../assets/icons/create-black-18dp.svg">
+        </td>
+        </tr>
+    `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: "Narayan Mahadevan",
+            _gender: "M",
+            _department: [
+                "Engineering",
+                "Finance"
+            ],
+            _salary: "500000",
+            _startDate: "29 Oct 2019",
+            _note: '',
+            _profilePic: "../assets/profile-images/Ellipse -2.png"
+        },
+        {
+            _name: "Amarpa",
+            _gender: "F",
+            _department: [
+                "Sales"
+            ],
+            _salary: "400000",
+            _startDate: "29 Oct 2019",
+            _note: '',
+            _profilePic: "../assets/profile-images/Ellipse -1.png"
+        }
+    ];
+    return employeePayrollListLocal;
+}
+
+const getDeptHtml = (deptList) =>{
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml}<div class = 'dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
